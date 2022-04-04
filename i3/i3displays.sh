a6d63570f34c71e3277891d728357ede9fb0e88f
Michael Rubin <mhr@neverthere.org>
2022-04-03 23:45:20 -0700

    Minor touch ups
    Fixing sound controls
    Making display script work for one or two monitors
    Improving resolution for laptop screen
    Adding bashrc for reference

--- i3/i3displays.sh
+++ i3/i3displays.sh
@@ -9,8 +9,12 @@ if [ $NUMDISPLAYS = "2" ]
 then
 	xrandr \
 	--output DP-1 --primary --mode 3840x2160 --pos 0x0 --rotate normal \
-	--output eDP-1 --mode 3840x2160 --pos 3840x0 --rotate normal \
+	--output eDP-1 --mode 2880x1620 --pos 3840x0 --rotate normal \
+	--output DP-2 --off
+else
+	xrandr \
+	--output eDP-1 --primary --mode 2880x1620 --pos 0x0 --rotate normal \
+	--output DP-1 --off
 	--output DP-2 --off
 fi
-	
 
