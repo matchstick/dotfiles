f7c779d9cd082d6f2e5b4843291e4d2773588efd
Michael Rubin <mhr@neverthere.org>
2022-04-28 21:11:25 -0700

    Updating config and i3displays
    i3displays now is more than a broken if statement
    config is more portable and robust

--- i3/i3displays.sh
+++ i3/i3displays.sh
@@ -1,22 +1,28 @@
 #!/bin/sh
 
-# Test to see if the laptop is connected to an external display. 
-# If so we want the external to be primary and on the left. 
+# Test to see if the laptop is connected to an external display.
+# If so we want the external to be primary and on the left.
+
+# This script is not portable but meant for my laptop setup.
 
 NUMDISPLAYS=`xrandr  | grep " connected " | wc -l`
 
-if [ $NUMDISPLAYS = "2" ]
-then
+case $NUMDISPLAYS in
+"2")
 	xrandr --output eDP1 --mode 3840x2160 --pos 3840x0 --rotate normal \
 	--output DP1 --primary --mode 3840x2160 --pos 0x0 --rotate normal \
 	--output DP2 --off \
 	--output DP3 --off \
 	--output VIRTUAL1 --off
-else
+	;;
+"1")
 	xrandr \
 	--output eDP1 --primary --mode 2880x1620 --pos 0x0 --rotate normal \
 	--output DP1 --off \
 	--output DP2 --off \
 	--output DP3 --off \
 	--output VIRTUAL1 --off
-fi
+	;;
+*)
+	;;
+esac
